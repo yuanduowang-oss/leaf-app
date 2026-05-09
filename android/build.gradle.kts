a46ff1c04ae663357d443012569fa98f2e879aa7
yuanduowang-oss <yuanduowang@gmail.com>
2026-05-09 19:55:05 +0800

    chore: add android/build.gradle.kts

---
+++ android/build.gradle.kts
@@ -0,0 +1,8 @@
+// android/build.gradle.kts 鈥?鍙跺瓙 APP
+// Flutter 3.41+ requires the Android v2 embedding with no legacy allprojects/subprojects blocks.
+
+plugins {
+    id("com.android.application") version "8.11.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
+}
